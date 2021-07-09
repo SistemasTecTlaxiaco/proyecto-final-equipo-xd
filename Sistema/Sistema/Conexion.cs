2d4461cf53575e8d4ec7ccae83e95df1fe70fdee
salathiel1999 <55575006+salathiel1999@users.noreply.github.com>
2021-07-09 15:50:24 -0500

    Add files via upload

---
+++ Sistema/Sistema/Conexion.cs
@@ -0,0 +1,75 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema
+{
+    class Conexion
+    {
+        public static MySqlConnection getConexion()
+        {
+            string servidor = "localhost";
+            string puerto = "3306";
+            string usuario = "root";
+            string password = "tecnologico123";
+            string bd = "mercadito";
+
+            string cadenaConexion = "server=" + servidor + "; port=" + puerto + "; user id=" + usuario + "; password="+password + "; database="+bd;
+
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+
+            return conexion;
+
+        }
+        public static MySqlConnection conexion()
+        {
+            string servidor = "localhost";
+            string bd = "tienda";
+            string usuario = "root";
+            string password = "tecnologico123";
+
+            string cadenaConexion = "Database=" + bd + "; Data Source=" + servidor + "; User Id= " + usuario + "; Password=" + password + "";
+
+            try
+            {
+                MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
+                return conexionBD;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+
+
+
+        public MySqlConnection conexio()
+        {
+            string servidor = "localhost";
+            string bd = "tienda";
+            string usuario = "root";
+            string password = "tecnologico123";
+
+            string cadenaConexion = "Database=" + bd + "; Data Source=" + servidor + "; User Id= " + usuario + "; Password=" + password + "";
+
+            try
+            {
+                MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
+                return conexionBD;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+    }
+
+}
+
---
+++ Sistema/Sistema/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string dato = txtCampo.Text;
+            cargarTabla(dato);
+        }
+
+        private void cargarTabla(string dato)
+        {
+            List<Productos> lista = new List<Productos>();
+            CtrlProductos _ctrlProductos = new CtrlProductos();
+            dataGridView1.DataSource = _ctrlProductos.consulta(dato);
+        }
+    }
+}
